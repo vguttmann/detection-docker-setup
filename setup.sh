d5c2e78fe98972c2460a273f018bc87f0315255f
vguttmann <61903989+vguttmann@users.noreply.github.com>
2024-07-30 15:22:23 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/arm64/cuda-ubuntu2204.pin
 mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/12.5.1/local_installers/cuda-tegra-repo-ubuntu2204-12-5-local_12.5.1-1_arm64.deb
-sudo dpkg -i cuda-tegra-repo-ubuntu2204-12-5-local_12.5.1-1_arm64.deb
+dpkg -i cuda-tegra-repo-ubuntu2204-12-5-local_12.5.1-1_arm64.deb
 cp /var/cuda-tegra-repo-ubuntu2204-12-5-local/cuda-*-keyring.gpg /usr/share/keyrings/
 apt update
 apt install -y libgstreamer1.0-dev libde265-0 libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio cuda-toolkit-12-5 nano fzf
